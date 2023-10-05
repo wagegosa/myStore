61047b502be64d4ff5604007ca6be8b49a646ee3
Walter Gerardo Gonzalez Sanchez <wgonzalez@credicorpcapital.com>
2023-10-05 10:34:57 -0500

    Se agrega metodo post.

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -30,4 +30,12 @@ router.get('/:producId', (req, res) =>{
   )
 });
 
+router.post('/', (req, res) => {
+  const body =req.body;
+   res.json({
+    message: 'created',
+    data: body
+   });
+});
+
 module.exports = router;
