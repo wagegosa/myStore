28f8b11d54eff265ce41a0bb1124ea7cdeea0e27
Walter Gerardo Gonzalez Sanchez <wgonzalez@credicorpcapital.com>
2023-10-16 07:37:23 -0500

    Se modifican las rutas con validaciones y try cahs.

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,63 +1,82 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const productsService = require('../services/products.service');
+const validatorHandler = require('../middleware/validator.hadler');
+const { uctSchema, updateProductSchema, getProductSchema, createProductSchema } = require('../schemas/product.schema');
 
 const router = express.Router();
+const service = new productsService();
+
 // porductos
-router.get('/', (req, res) =>{
-  const products = [];
-  const { size } = req.query;
-  const limit =size || 10;
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      img: faker.image.url(),
-    });
+router.get('/', async (req, res, next) =>{
+  try {
+    const product = await service.find();
+    if (!product) {
+      res.json({
+        message: 'No se encontro registro'
+      })
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (error) {
+    next(error);
   }
-  res.json(products);
 });
 router.get('/filter', (req, res) =>{
   res.send('Soy un filtro');
 });
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  if (id === '999') {
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      if (!product) {
+        res.json({
+          message: 'No se encontro registro'
+        })
+      } else {
+        res.status(200).json(product);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  }
+);
+
+router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id', async  (req, res) => {
+  try {
+    const { id } = req.params;
+    const rta = await  service.delete(id, body);
+    res.json(rta);
+  } catch (error) {
     res.status(404).json({
-      message: 'not found'
-    })
-  } else {
-    res.status(200).json({
-      id,
-      name: 'Product 2',
-      price: 2000
+      message: error.message
     });
   }
-});
-
-router.post('/', (req, res) => {
-  const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body
-  });
-});
-
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'update',
-    data: body,
-    id,
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    message: 'deleted',
-    id,
-  });
 });
 
 module.exports = router;
