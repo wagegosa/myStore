4e78d59af707dbfe30231750daa3af2d6d8e52d1
Walter Gerardo Gonzalez Sanchez <wgonzalez@credicorpcapital.com>
2023-10-16 19:14:43 -0500

    Se agrga los cors y las ip¨s que pueden consumir nuestro servicios.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handler')
 
@@ -6,6 +7,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whiteList =['http://127.0.0.1:5500/frontend.html', 'http://localhost:8080/My_app', 'http://MyApp.co'];
+const option = {
+  orign: (origin, callback) =>{
+    if (whiteList.includes(origin) || !origin ) {
+      callback(null, true);
+    } else {
+      callback(new Error)
+    }
+  }
+};
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('Hola mi server en express')
